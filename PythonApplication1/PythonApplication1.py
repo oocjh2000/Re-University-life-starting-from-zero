41c5eaf775b1b0c86a8bd04814ed68d75eee1032
jaehun cha <oocjh200@gmail.com>
2019-03-12 12:47:29 +0900

    초기 커밋.

---
+++ PythonApplication1/PythonApplication1.py
@@ -0,0 +1,16 @@
+principal = int(input("원금입력 : "))
+interest = float(input("이자율 입력(%) : "))
+interest=interest*0.01
+year= int(input("상환기간 입력 : "))
+res=principal
+for year in range(24):
+    res+= res * interest
+
+print("---------------------------------------------")
+print("원금 : "+ str(principal))
+print("이자율 : "+str(interest*100))
+print("상환기간 : "+str(year+1))
+print("상환해야할금액 : "+str(res))
+
+
+  
