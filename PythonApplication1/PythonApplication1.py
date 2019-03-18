f12ffac86f948ba970af22264f7dce3ae84d8308
jaehun cha <oocjh200@gmail.com>
2019-03-18 15:33:35 +0900

    22

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,4 +1,19 @@
-name=str(input("이름을 입력해 주세요"))
-print(str+"님 반갑습니다.")
-age=int(input("how old are you?"))
-print("10년뒤에는"+str(age+10)+"살이 되시는군요")
+price=int(input("물건값을 입력하세요"))
+thou=int(input("1000won : "))
+five=int(input("500won : "))
+one=int(input("100won : "))
+total=(thou*1000)+(five*500)+(one*100)
+res=total-price
+thou=res//1000
+res=res%1000
+
+five=res//500
+res=res%500
+
+one=res//100
+res=res%100
+
+ten=res//10
+res=res%10
+
+print("1000 is "+str(thou)+" 500 is :"+str(five)+" 100 is :"+str(one)+" 10 is :"+str(ten))
