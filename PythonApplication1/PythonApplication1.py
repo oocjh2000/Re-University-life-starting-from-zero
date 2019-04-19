ed139554cf28c96394f755f05557f809d989af4d
oocjh2000 <oocjh200@gmail.com>
2019-04-20 03:50:20 +0900

    Update PythonApplication1.py
    잘못짠거 수정

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -2,9 +2,9 @@ def bokun(n):
     sum=0
     if n%17>8:
         n+=17
-        n=n*(n//3)
+        n=17*(n//17)
     else:
-     n=n*(n//3)
+     n=17*(n//17)
     for i in range(1,n):
         if i%3==0:
             continue
