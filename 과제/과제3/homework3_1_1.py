33cf97152dea3392716c837ecc16779b2cc6b407
jaehun cha <oocjh200@gmail.com>
2019-06-03 16:41:48 +0900

    .

--- 과제/과제3/homework3_1_1.py
+++ 과제/과제3/homework3_1_1.py
@@ -4,11 +4,12 @@ import pickle
 saving_accounts=[]
 check_accounts=[]
 for i in range(5):
-    var=bank.SavingsAccount('sav'+str(i),i,(i+1)+100000,i+1*6.3)
+    var=bank.SavingsAccount('sav'+str(i),i,random.randint(10000,100000),random.randint(1,10)*0.01)
     saving_accounts.append(var)
-    var=bank.CheckingAccount('che'+str(i),i+5,(i+1)*10000)
+    var=bank.CheckingAccount('che'+str(i),i,random.randint(10000,100000))
     check_accounts.append(var)
 f=open('과제3\saving.bin','wb')
 pickle.dump(saving_accounts,f)
 f=open('과제3\checking.bin','wb')
 pickle.dump(check_accounts,f)
+print('계좌정보가 저장되었습니다 homework3_1_2를 실행해주세요')
